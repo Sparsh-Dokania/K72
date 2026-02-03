ae4d8a3fb93f480aa06576c321924771b41645be
Sparsh-Dokania <sparshdokania2004@gmail.com>
2026-02-03 20:23:08 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -8,17 +8,16 @@ const Agence = () => {
   const imageRef = useRef(null)
 
   const imageArray = [
-    'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
-    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
-    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
-    'https://k72.ca/images/teamMembers/HugoJoseph_480x640.jpg?w=480&h=640&fit=crop&s=f152025b8a59b062d1e7978b5d6544c3',
-    'https://k72.ca/images/teamMembers/Lawrence_480x640.jpg?w=480&h=640&fit=crop&s=0a878205586092164001a9afe0ef4007',
-    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
-    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
-    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
-    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
-    'https://k72.ca/images/teamMembers/SophieA_480x640.jpg?w=480&h=640&fit=crop&s=fcb556060c29623e706dfbc4eeca87ac',
-    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+    'public/images/ag1.jpg',
+    'public/images/ag2.jpg',
+    'public/images/ag3.jpg',
+    'public/images/ag4.jpg',
+    'public/images/ag5.jpg',
+    'public/images/ag6.jpg',
+    'public/images/ag7.jpg',
+    'public/images/ag8.jpg',
+    'public/images/ag9.jpg',
+    'public/images/ag10.jpg'
   ]
 
   gsap.registerPlugin(ScrollTrigger)
@@ -51,9 +50,7 @@ const Agence = () => {
       <div className="section1 py-1">
         <div ref={imageDivRef} className=" absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw] top-50 left-[30vw]">
           <img ref={imageRef}
-            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7
-"
-            alt=""
+            src="public/images/ag1.jpg"    alt=""
             className="rounded-4xl object-cover h-full w-full"
           />
         </div>

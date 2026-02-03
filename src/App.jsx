52c6092ca8b8e04af84a9556c1bc2be3277a9c84
Sparsh-Dokania <sparshdokania2004@gmail.com>
2026-02-03 22:10:45 +0530

    Refactor App, Home, and Projects components for improved structure and consistency; update headings for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,15 @@ import Navbar from './components/navigation/Navbar'
 import FullScreenNav from './components/navigation/FullScreenNav'
 
 const App = () => {
-
   return (
-    <div className='h-screen w-screen'>
-      <Navbar/>
-      <FullScreenNav/>
-     <Routes>
-      <Route path='/' element = {<Home/>} />
-      <Route path='/agence' element = {<Agence/>} />
-      <Route path='/projects' element = {<Projects/>} />
-     </Routes>
+    <div className='overflow-x-hidden'>
+      <Navbar />
+      <FullScreenNav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ import HomeHeroText from '../components/home/HomeHeroText.jsx'
 import HomeBottomText from '../components/home/HomeBottomText.jsx'
 
 const Home = () => {
-  return (
-    <div>
+   return (
+    <div className='text-white'>
       <div className='h-screen w-screen fixed'>
-        <Video/>
+        <Video />
       </div>
-      <div className='h-screen w-screen relative flex flex-col justify-between pb-5'>
-        <HomeHeroText/>
-        <HomeBottomText/>
+      <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
+        <HomeHeroText />
+        <HomeBottomText />
       </div>
     </div>
   )
